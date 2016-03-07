3f08a5dd409f56633df037f997574e56b6a2d60f
Bobby King <king.robertjoseph@gmail.com>
2016-03-07 16:19:41 -0500

    Add zip codes

---
+++ zip_codes.js
@@ -0,0 +1,724 @@
+zips = {
+  '10453': {
+    borough: 'Bronx',
+    neighborhood: 'Central Bronx'
+  },
+  '10457': {
+    borough: 'Bronx',
+    neighborhood: 'Central Bronx'
+  },
+  '10460': {
+    borough: 'Bronx',
+    neighborhood: 'Central Bronx'
+  },
+  '10458': {
+    borough: 'Bronx',
+    neighborhood: 'Bronx Park and Fordham'
+  },
+  '10467': {
+    borough: 'Bronx',
+    neighborhood: 'Bronx Park and Fordham'
+  },
+  '10468': {
+    borough: 'Bronx',
+    neighborhood: 'Bronx Park and Fordham'
+  },
+  '10451': {
+    borough: 'Bronx',
+    neighborhood: 'High Bridge and Morrisania'
+  },
+  '10452': {
+    borough: 'Bronx',
+    neighborhood: 'High Bridge and Morrisania'
+  },
+  '10456': {
+    borough: 'Bronx',
+    neighborhood: 'High Bridge and Morrisania'
+  },
+  '10454': {
+    borough: 'Bronx',
+    neighborhood: 'Hunts Point and Mott Haven'
+  },
+  '10455': {
+    borough: 'Bronx',
+    neighborhood: 'Hunts Point and Mott Haven'
+  },
+  '10459': {
+    borough: 'Bronx',
+    neighborhood: 'Hunts Point and Mott Haven'
+  },
+  '10474': {
+    borough: 'Bronx',
+    neighborhood: 'Hunts Point and Mott Haven'
+  },
+  '10463': {
+    borough: 'Bronx',
+    neighborhood: 'Kingsbridge and Riverside'
+  },
+  '10471': {
+    borough: 'Bronx',
+    neighborhood: 'Kingsbridge and Riverside'
+  },
+  '10466': {
+    borough: 'Bronx',
+    neighborhood: 'Northeast Bronx'
+  },
+  '10469': {
+    borough: 'Bronx',
+    neighborhood: 'Northeast Bronx'
+  },
+  '10470': {
+    borough: 'Bronx',
+    neighborhood: 'Northeast Bronx'
+  },
+  '10475': {
+    borough: 'Bronx',
+    neighborhood: 'Northeast Bronx'
+  },
+  '10461': {
+    borough: 'Bronx',
+    neighborhood: 'Southeast Bronx'
+  },
+  '10462': {
+    borough: 'Bronx',
+    neighborhood: 'Southeast Bronx'
+  },
+  '10464': {
+    borough: 'Bronx',
+    neighborhood: 'Southeast Bronx'
+  },
+  '10465': {
+    borough: 'Bronx',
+    neighborhood: 'Southeast Bronx'
+  },
+  '10472': {
+    borough: 'Bronx',
+    neighborhood: 'Southeast Bronx'
+  },
+  '10473': {
+    borough: 'Bronx',
+    neighborhood: 'Southeast Bronx'
+  },
+  '11212': {
+    borough: 'Brooklyn',
+    neighborhood: 'Central Brooklyn'
+  },
+  '11213': {
+    borough: 'Brooklyn',
+    neighborhood: 'Central Brooklyn'
+  },
+  '11216': {
+    borough: 'Brooklyn',
+    neighborhood: 'Central Brooklyn'
+  },
+  '11233': {
+    borough: 'Brooklyn',
+    neighborhood: 'Central Brooklyn'
+  },
+  '11238': {
+    borough: 'Brooklyn',
+    neighborhood: 'Central Brooklyn'
+  },
+  '11209': {
+    borough: 'Brooklyn',
+    neighborhood: 'Southwest Brooklyn'
+  },
+  '11214': {
+    borough: 'Brooklyn',
+    neighborhood: 'Southwest Brooklyn'
+  },
+  '11228': {
+    borough: 'Brooklyn',
+    neighborhood: 'Southwest Brooklyn'
+  },
+  '11204': {
+    borough: 'Brooklyn',
+    neighborhood: 'Borough Park'
+  },
+  '11218': {
+    borough: 'Brooklyn',
+    neighborhood: 'Borough Park'
+  },
+  '11219': {
+    borough: 'Brooklyn',
+    neighborhood: 'Borough Park'
+  },
+  '11230': {
+    borough: 'Brooklyn',
+    neighborhood: 'Borough Park'
+  },
+  '11234': {
+    borough: 'Brooklyn',
+    neighborhood: 'Canarsie and Flatlands'
+  },
+  '11236': {
+    borough: 'Brooklyn',
+    neighborhood: 'Canarsie and Flatlands'
+  },
+  '11239': {
+    borough: 'Brooklyn',
+    neighborhood: 'Canarsie and Flatlands'
+  },
+  '11223': {
+    borough: 'Brooklyn',
+    neighborhood: 'Southern Brooklyn'
+  },
+  '11224': {
+    borough: 'Brooklyn',
+    neighborhood: 'Southern Brooklyn'
+  },
+  '11229': {
+    borough: 'Brooklyn',
+    neighborhood: 'Southern Brooklyn'
+  },
+  '11235': {
+    borough: 'Brooklyn',
+    neighborhood: 'Southern Brooklyn'
+  },
+  '11201': {
+    borough: 'Brooklyn',
+    neighborhood: 'Northwest Brooklyn'
+  },
+  '11205': {
+    borough: 'Brooklyn',
+    neighborhood: 'Northwest Brooklyn'
+  },
+  '11215': {
+    borough: 'Brooklyn',
+    neighborhood: 'Northwest Brooklyn'
+  },
+  '11217': {
+    borough: 'Brooklyn',
+    neighborhood: 'Northwest Brooklyn'
+  },
+  '11231': {
+    borough: 'Brooklyn',
+    neighborhood: 'Northwest Brooklyn'
+  },
+  '11203': {
+    borough: 'Brooklyn',
+    neighborhood: 'Flatbust'
+  },
+  '11210': {
+    borough: 'Brooklyn',
+    neighborhood: 'Flatbust'
+  },
+  '11225': {
+    borough: 'Brooklyn',
+    neighborhood: 'Flatbust'
+  },
+  '11226': {
+    borough: 'Brooklyn',
+    neighborhood: 'Flatbust'
+  },
+  '11207': {
+    borough: 'Brooklyn',
+    neighborhood: 'East New York and New Lots'
+  },
+  '11208': {
+    borough: 'Brooklyn',
+    neighborhood: 'East New York and New Lots'
+  },
+  '11211': {
+    borough: 'Brooklyn',
+    neighborhood: 'Greenpoint'
+  },
+  '11222': {
+    borough: 'Brooklyn',
+    neighborhood: 'Greenpoint'
+  },
+  '11220': {
+    borough: 'Brooklyn',
+    neighborhood: 'Sunset Park'
+  },
+  '11232': {
+    borough: 'Brooklyn',
+    neighborhood: 'Sunset Park'
+  },
+  '11206': {
+    Borough: 'Brooklyn',
+    neighborhood: 'Buchwick and Williamsburg'
+  },
+  '11221': {
+    Borough: 'Brooklyn',
+    neighborhood: 'Buchwick and Williamsburg'
+  },
+  '11237': {
+    Borough: 'Brooklyn',
+    neighborhood: 'Buchwick and Williamsburg'
+  },
+  '10026': {
+    borough: 'Manhattan',
+    neighborhood: 'Central Harlem'
+  },
+  '10027': {
+    borough: 'Manhattan',
+    neighborhood: 'Central Harlem'
+  },
+  '10030': {
+    borough: 'Manhattan',
+    neighborhood: 'Central Harlem'
+  },
+  '10037': {
+    borough: 'Manhattan',
+    neighborhood: 'Central Harlem'
+  },
+  '10039': {
+    borough: 'Manhattan',
+    neighborhood: 'Central Harlem'
+  },
+  '10001': {
+    borough: 'Manhattan',
+    neighborhood: 'Chelsea and Clinton'
+  },
+  '10011': {
+    borough: 'Manhattan',
+    neighborhood: 'Chelsea and Clinton'
+  },
+  '10018': {
+    borough: 'Manhattan',
+    neighborhood: 'Chelsea and Clinton'
+  },
+  '10019': {
+    borough: 'Manhattan',
+    neighborhood: 'Chelsea and Clinton'
+  },
+  '10020': {
+    borough: 'Manhattan',
+    neighborhood: 'Chelsea and Clinton'
+  },
+  '10036': {
+    borough: 'Manhattan',
+    neighborhood: 'Chelsea and Clinton'
+  },
+  '10029': {
+    borough: 'Manhattan',
+    neighborhood: 'East Harlem'
+  },
+  '10035': {
+    borough: 'Manhattan',
+    neighborhood: 'East Harlem'
+  },
+  '10010': {
+    borough: 'Manhattan',
+    neighborhood: 'Grammercy Park and Murray Hill'
+  },
+  '10016': {
+    borough: 'Manhattan',
+    neighborhood: 'Grammercy Park and Murray Hill'
+  },
+  '10017': {
+    borough: 'Manhattan',
+    neighborhood: 'Grammercy Park and Murray Hill'
+  },
+  '10022': {
+    borough: 'Manhattan',
+    neighborhood: 'Grammercy Park and Murray Hill'
+  },
+  '10012': {
+    borough: 'Manhattan',
+    neighborhood: 'Greenwich Village and Soho'
+  },
+  '10013': {
+    borough: 'Manhattan',
+    neighborhood: 'Greenwich Village and Soho'
+  },
+  '10014': {
+    borough: 'Manhattan',
+    neighborhood: 'Greenwich Village and Soho'
+  },
+  '10004': {
+    borough: 'Manhattan',
+    neighborhood: 'Lower Manhattan'
+  },
+  '10005': {
+    borough: 'Manhattan',
+    neighborhood: 'Lower Manhattan'
+  },
+  '10006': {
+    borough: 'Manhattan',
+    neighborhood: 'Lower Manhattan'
+  },
+  '10007': {
+    borough: 'Manhattan',
+    neighborhood: 'Lower Manhattan'
+  },
+  '10038': {
+    borough: 'Manhattan',
+    neighborhood: 'Lower Manhattan'
+  },
+  '10280': {
+    borough: 'Manhattan',
+    neighborhood: 'Lower Manhattan'
+  },
+  '10002': {
+    borough: 'Manhattan',
+    neighborhood: 'Lower East Side'
+  },
+  '10003': {
+    borough: 'Manhattan',
+    neighborhood: 'Lower East Side'
+  },
+  '10009': {
+    borough: 'Manhattan',
+    neighborhood: 'Lower East Side'
+  },
+  '10021': {
+    borough: 'Manhattan',
+    neighborhood: 'Upper East Side'
+  },
+  '10028': {
+    borough: 'Manhattan',
+    neighborhood: 'Upper East Side'
+  },
+  '10044': {
+    borough: 'Manhattan',
+    neighborhood: 'Upper East Side'
+  },
+  '10065': {
+    borough: 'Manhattan',
+    neighborhood: 'Upper East Side'
+  },
+  '10075': {
+    borough: 'Manhattan',
+    neighborhood: 'Upper East Side'
+  },
+  '10128': {
+    borough: 'Manhattan',
+    neighborhood: 'Upper East Side'
+  },
+  '10023': {
+    borough: 'Manhattan',
+    neighborhood: 'Upper West Side'
+  },
+  '10024': {
+    borough: 'Manhattan',
+    neighborhood: 'Upper West Side'
+  },
+  '10025': {
+    borough: 'Manhattan',
+    neighborhood: 'Upper West Side'
+  },
+  '10031': {
+    borough: 'Manhattan',
+    neighborhood: 'Inwood and Washington Heights'
+  },
+  '10032': {
+    borough: 'Manhattan',
+    neighborhood: 'Inwood and Washington Heights'
+  },
+  '10033': {
+    borough: 'Manhattan',
+    neighborhood: 'Inwood and Washington Heights'
+  },
+  '10034': {
+    borough: 'Manhattan',
+    neighborhood: 'Inwood and Washington Heights'
+  },
+  '10040': {
+    borough: 'Manhattan',
+    neighborhood: 'Inwood and Washington Heights'
+  },
+  '11361': {
+    borough: 'Queens',
+    neighborhood: 'Northeast Queens'
+  },
+  '11362': {
+    borough: 'Queens',
+    neighborhood: 'Northeast Queens'
+  },
+  '11363': {
+    borough: 'Queens',
+    neighborhood: 'Northeast Queens'
+  },
+  '11364': {
+    borough: 'Queens',
+    neighborhood: 'Northeast Queens'
+  },
+  '11354': {
+    borough: 'Queens',
+    neighborhood: 'North Queens'
+  },
+  '11355': {
+    borough: 'Queens',
+    neighborhood: 'North Queens'
+  },
+  '11356': {
+    borough: 'Queens',
+    neighborhood: 'North Queens'
+  },
+  '11357': {
+    borough: 'Queens',
+    neighborhood: 'North Queens'
+  },
+  '11358': {
+    borough: 'Queens',
+    neighborhood: 'North Queens'
+  },
+  '11359': {
+    borough: 'Queens',
+    neighborhood: 'North Queens'
+  },
+  '11360': {
+    borough: 'Queens',
+    neighborhood: 'North Queens'
+  },
+  '11365': {
+    borough: 'Queens',
+    neighborhood: 'Central Queens'
+  },
+  '11366': {
+    borough: 'Queens',
+    neighborhood: 'Central Queens'
+  },
+  '11367': {
+    borough: 'Queens',
+    neighborhood: 'Central Queens'
+  },
+  '11412': {
+    borough: 'Queens',
+    neighborhood: 'Jamaica'
+  },
+  '11423': {
+    borough: 'Queens',
+    neighborhood: 'Jamaica'
+  },
+  '11432': {
+    borough: 'Queens',
+    neighborhood: 'Jamaica'
+  },
+  '11433': {
+    borough: 'Queens',
+    neighborhood: 'Jamaica'
+  },
+  '11434': {
+    borough: 'Queens',
+    neighborhood: 'Jamaica'
+  },
+  '11435': {
+    borough: 'Queens',
+    neighborhood: 'Jamaica'
+  },
+  '11436': {
+    borough: 'Queens',
+    neighborhood: 'Jamaica'
+  },
+  '11101': {
+    borough: 'Queens',
+    neighborhood: 'Northwest Queens'
+  },
+  '11102': {
+    borough: 'Queens',
+    neighborhood: 'Northwest Queens'
+  },
+  '11103': {
+    borough: 'Queens',
+    neighborhood: 'Northwest Queens'
+  },
+  '11104': {
+    borough: 'Queens',
+    neighborhood: 'Northwest Queens'
+  },
+  '11105': {
+    borough: 'Queens',
+    neighborhood: 'Northwest Queens'
+  },
+  '11106': {
+    borough: 'Queens',
+    neighborhood: 'Northwest Queens'
+  },
+  '11374': {
+    borough: 'Queens',
+    neighborhood: 'West Central Queens'
+  },
+  '11375': {
+    borough: 'Queens',
+    neighborhood: 'West Central Queens'
+  },
+  '11379': {
+    borough: 'Queens',
+    neighborhood: 'West Central Queens'
+  },
+  '11385': {
+    borough: 'Queens',
+    neighborhood: 'West Central Queens'
+  },
+  '11691': {
+    borough: 'Queens',
+    neighborhood: 'Rockaways'
+  },
+  '11692': {
+    borough: 'Queens',
+    neighborhood: 'Rockaways'
+  },
+  '11693': {
+    borough: 'Queens',
+    neighborhood: 'Rockaways'
+  },
+  '11694': {
+    borough: 'Queens',
+    neighborhood: 'Rockaways'
+  },
+  '11695': {
+    borough: 'Queens',
+    neighborhood: 'Rockaways'
+  },
+  '11697': {
+    borough: 'Queens',
+    neighborhood: 'Rockaways'
+  },
+  '11004': {
+    borough: 'Queens',
+    neighborhood: 'Southeast Queens'
+  },
+  '11005': {
+    borough: 'Queens',
+    neighborhood: 'Southeast Queens'
+  },
+  '11411': {
+    borough: 'Queens',
+    neighborhood: 'Southeast Queens'
+  },
+  '11413': {
+    borough: 'Queens',
+    neighborhood: 'Southeast Queens'
+  },
+  '11422': {
+    borough: 'Queens',
+    neighborhood: 'Southeast Queens'
+  },
+  '11426': {
+    borough: 'Queens',
+    neighborhood: 'Southeast Queens'
+  },
+  '11427': {
+    borough: 'Queens',
+    neighborhood: 'Southeast Queens'
+  },
+  '11428': {
+    borough: 'Queens',
+    neighborhood: 'Southeast Queens'
+  },
+  '11429': {
+    borough: 'Queens',
+    neighborhood: 'Southeast Queens'
+  },
+  '11414': {
+    Borough: 'Queens',
+    neighborhood: 'Southwest Queens'
+  },
+  '11415': {
+    Borough: 'Queens',
+    neighborhood: 'Southwest Queens'
+  },
+  '11416': {
+    Borough: 'Queens',
+    neighborhood: 'Southwest Queens'
+  },
+  '11417': {
+    Borough: 'Queens',
+    neighborhood: 'Southwest Queens'
+  },
+  '11418': {
+    Borough: 'Queens',
+    neighborhood: 'Southwest Queens'
+  },
+  '11419': {
+    Borough: 'Queens',
+    neighborhood: 'Southwest Queens'
+  },
+  '11420': {
+    Borough: 'Queens',
+    neighborhood: 'Southwest Queens'
+  },
+  '11421': {
+    Borough: 'Queens',
+    neighborhood: 'Southwest Queens'
+  },
+  '11368': {
+    borough: 'Queens',
+    neighborhood: 'West Queens'
+  },
+  '11369': {
+    borough: 'Queens',
+    neighborhood: 'West Queens'
+  },
+  '11370': {
+    borough: 'Queens',
+    neighborhood: 'West Queens'
+  },
+  '11372': {
+    borough: 'Queens',
+    neighborhood: 'West Queens'
+  },
+  '11373': {
+    borough: 'Queens',
+    neighborhood: 'West Queens'
+  },
+  '11377': {
+    borough: 'Queens',
+    neighborhood: 'West Queens'
+  },
+  '11378': {
+    borough: 'Queens',
+    neighborhood: 'West Queens'
+  },
+  '10302': {
+    borough: 'Staten Island',
+    neighborhood: 'Port Richmond'
+  },
+  '10303': {
+    borough: 'Staten Island',
+    neighborhood: 'Port Richmond'
+  },
+  '10310': {
+    borough: 'Staten Island',
+    neighborhood: 'Port Richmond'
+  },
+  '10306': {
+    borough: 'Staten Island',
+    neighborhood: 'South Shore'
+  },
+  '10307': {
+    borough: 'Staten Island',
+    neighborhood: 'South Shore'
+  },
+  '10308': {
+    borough: 'Staten Island',
+    neighborhood: 'South Shore'
+  },
+  '10309': {
+    borough: 'Staten Island',
+    neighborhood: 'South Shore'
+  },
+  '10312': {
+    borough: 'Staten Island',
+    neighborhood: 'South Shore'
+  },
+  '10301': {
+    borough: 'Staten Island',
+    neighborhood: 'Stapleton and St. George'
+  },
+  '10304': {
+    borough: 'Staten Island',
+    neighborhood: 'Stapleton and St. George'
+  },
+  '10305': {
+    borough: 'Staten Island',
+    neighborhood: 'Stapleton and St. George'
+  },
+  '10314': {
+    borough: 'Staten Island',
+    neighborhood: 'Mid-Island'
+  }
+};
+
+module.exports = zips;
+
+
+
+
+
+
+
+
